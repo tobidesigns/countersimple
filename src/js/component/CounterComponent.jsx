f4f71ad26e2ca8af5ae30f381fc81339cc35674f
Tobi <tobiraheem@gmail.com>
2023-04-01 11:34:44 +0000

    useState imported and function

---
+++ src/js/component/CounterComponent.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react'
+
+const CounterComponent = (props) => {
+  return (
+    <div>
+        <h1>{props.value}</h1>
+    </div>
+  )
+}
+
+export default CounterComponent
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import CounterComponent from "./CounterComponent.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	let [seconds, setSeconds] = useState(0)
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+		  setSeconds(prevSeconds => prevSeconds + 1);
+		}, 1000);
+	
+		return () => clearInterval(intervalId);
+	  }, []);
+
+
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<CounterComponent value={seconds}/>
 	);
 };
 
